b1bea5ec8e18f74b902eeea8523f5d5ab706bb30
hesarnurramaduloh <hesarnur@gmail.com>
2017-02-10 13:00:33 +0700

    v.1.0

---
+++ Tugas 1/index.php
@@ -0,0 +1,9 @@
+<?php
+echo ("Nama  : Hesar Nur Ramaduloh");
+echo "<br>";
+echo ("Kelas : 14 TIF RM"); 
+echo "<br>";
+echo ("NPM   : 14111218");
+echo "<br>";
+echo ("<<< Sekolah Tinggi Teknologi Bandung >>>");
+?>
